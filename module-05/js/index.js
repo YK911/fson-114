2118c82c3d63b9bbaf25e42ffb3d39fe5d572ab3
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2024-11-23 01:01:57 +0200

    finished module 05 lesson 02

--- module-05/js/index.js
+++ module-05/js/index.js
@@ -1,6 +1,6 @@
-const paymentForm = window.document.forms.paymentForm;
+// const paymentForm = window.document.forms.paymentForm;
 
-paymentForm.addEventListener('submit', onSubmit);
+// paymentForm.addEventListener('submit', onSubmit);
 
 function onSubmit(event) {
   event.preventDefault();
@@ -19,11 +19,23 @@ function onSubmit(event) {
   form.reset();
 }
 
-const rangeEl = document.querySelector('input[type="range"]');
+// const rangeEl = document.querySelector('input[type="range"]');
 
-rangeEl.addEventListener('input', onInputChange);
+// rangeEl.addEventListener('input', onInputChange);
 
-function onInputChange(event) {
-  const value = event.target.value;
-  resultValue.textContent = value;
+// function onInputChange(event) {
+//   const value = event.target.value;
+//   resultValue.textContent = value;
+// }
+
+// Modal form
+const modalBtnOpen = document.querySelector('.js-open-modal');
+const modalBtnClose = document.querySelector('.js-close-modal');
+const modal = document.querySelector('.js-modal');
+
+modalBtnOpen.addEventListener('click', toggleModal);
+modalBtnClose.addEventListener('click', toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle('is-open');
 }
